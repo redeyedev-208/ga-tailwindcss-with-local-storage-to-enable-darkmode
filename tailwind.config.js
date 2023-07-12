e8c1bfaf63a5ba2137b954f72cfc62f56248a979
gilaguilar <redeyedev.208@gmail.com>
2023-07-12 17:30:40 -0600

    feat: added deploy config to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./*.html'],
+  content: ['./*.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
